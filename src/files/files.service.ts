21e84ac6589b36cb5de9c58957bf8dbfb4857b5d
bulian1311 <bulian1311@gmail.com>
2021-06-11 15:21:12 +0300

    Upload and serve image files.

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,33 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { format } from 'date-fns';
-import { path } from 'app-root-path';
-import { ensureDir, writeFile } from 'fs-extra';
+import {format} from 'date-fns'
+import {path} from 'app-root-path'
+import {ensureDir, writeFile} from 'fs-extra'
 import { FileElementResponse } from './dto/file-element.response';
-import * as sharp from 'sharp';
+import * as sharp from 'sharp'
+import { MFile } from './mfile.class';
 
 @Injectable()
 export class FilesService {
-  async saveFiles(
-    files: Express.Multer.File[],
-  ): Promise<FileElementResponse[]> {
+
+  async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
     const dateFolder = format(new Date(), 'yyy-MM-dd');
     const uploadFolder = `${path}/uploads/${dateFolder}`;
     await ensureDir(uploadFolder);
 
-    const res: FileElementResponse[] = [];
+    const res: FileElementResponse[] = []
 
-    for (const file of files) {
+    for(const file of files) {
       await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer);
-      res.push({
-        url: `${dateFolder}/${file.originalname}`,
-        name: file.originalname,
-      });
+      res.push({url: `${dateFolder}/${file.originalname}`, name: file.originalname})
     }
 
     return res;
   }
 
   convertToWebP(file: Buffer) {
-    return sharp(file).webp().toBuffer();
+    return sharp(file)
+      .webp()
+      .toBuffer();
   }
 }
